7e602bae0889e8565fcfd9a59c39a086425c8b76
HunnuH <88477795+HunnuH@users.noreply.github.com>
2021-12-01 15:33:17 +0900

    12..01
    12.01 view, controller

--- work/androidwork/Final_Project/app/src/main/java/pro/app/fnl/Frag2.java
+++ work/androidwork/Final_Project/app/src/main/java/pro/app/fnl/Frag2.java
@@ -4,23 +4,54 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class Frag2 extends Fragment // Fragment 클래스를 상속받아야한다
-{
-
+public class Frag2 extends Fragment {
     private View view;
-
+    WebView web;
+    EditText text;
+    Button conn;
+    ViewGroup viewGroup;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
         view = inflater.inflate(R.layout.frag2,container,false);
+        web =  view.findViewById(R.id.webview);
+        conn = view.findViewById(R.id.web_id);
+        text = view.findViewById(R.id.web_text);
 
+        WebSettings webSettings = web.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        web.setWebViewClient(new WebViewClient());
+        webSettings.setUseWideViewPort(true);
+        webSettings.setBuiltInZoomControls(false);
+        webSettings.setSaveFormData(false);
+        web.loadUrl("http://naver.com");
+
+
+        conn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                web.loadUrl(text.getText().toString());
+            }
+        });
         return view;
+    }
+    private class ViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(final WebView view, final String url) {
+            view.loadUrl(url);
+            return true;
+        }
     }
 }
