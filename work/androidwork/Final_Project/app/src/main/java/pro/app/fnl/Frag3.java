6514cc3b762fbfb2678982ba0f2a6dfba1286f36
HunnuH <88477795+HunnuH@users.noreply.github.com>
2021-12-01 23:25:15 +0900

    12.01
    기능 구현

--- work/androidwork/Final_Project/app/src/main/java/pro/app/fnl/Frag3.java
+++ work/androidwork/Final_Project/app/src/main/java/pro/app/fnl/Frag3.java
@@ -1,10 +1,14 @@
 package pro.app.fnl;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,21 +19,40 @@ import com.bumptech.glide.Glide;
 public class Frag3 extends Fragment  {
     private View view;
     ViewGroup viewGroup;
-    ImageView fan_img;
+    TextView call_txt, pow_txt;
+    ImageButton call_btn, pow_btn;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         viewGroup = (ViewGroup)inflater.inflate(R.layout.frag1,container,false);
         view = inflater.inflate(R.layout.frag3,container,false);
-        fan_img = (ImageView)viewGroup.findViewById(R.id.fan_img);
+        pow_txt = view.findViewById(R.id.pow_txt);
+        pow_btn = view.findViewById(R.id.pow_btn);
+        call_txt = view.findViewById(R.id.call_txt);
+        call_btn = view.findViewById(R.id.call_btn);
 
+
+        pow_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                pow_txt.setText("OFF");
+
+            }
+        });
+
+        call_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("119"));
+                startActivity(intent);
+                } catch (Exception e) {
+
+                }
+            }
+        });
         return view;
     }
-        public void setImage(int index) {
-            if (index == 0) {
-                 fan_img.setImageResource(R.drawable.fan0);
-            } else if (index == 1) {
-                 fan_img.setImageResource(R.drawable.fan1);
-            }
-        }
+
 }
