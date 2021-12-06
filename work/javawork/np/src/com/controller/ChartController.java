5e450b2732a892772957f924191a7e57db978b62
JUNBEOM PARK <junbum0674@gmail.com>
2021-12-06 09:27:26 +0900

    21.12.06 junbeom
    rchart total graph update

--- work/javawork/np/src/com/controller/ChartController.java
+++ work/javawork/np/src/com/controller/ChartController.java
@@ -33,11 +33,11 @@ public class ChartController {
 	
 	@RequestMapping("/rcharttemp.mc")
 	@ResponseBody
-	public void ruu(HttpServletResponse response) throws IOException, RserveException, REXPMismatchException {
+	public void ruu_temp(HttpServletResponse response) throws IOException, RserveException, REXPMismatchException {
 		response.setContentType("text/json;charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		
-		RConnection rconn = new RConnection("192.168.0.158");
+		RConnection rconn = new RConnection("192.168.0.29");
 		rconn.setStringEncoding("utf8");
 
 		rconn.eval("source('C:/logs/final_test.R',encoding='UTF-8')");
@@ -167,6 +167,54 @@ public class ChartController {
 //		out.print(ja.toJSONString());
 //		out.close();
 //	}
+	
+		
+		@RequestMapping("/rchart_tot.mc")
+		@ResponseBody
+		public void ruu_tot(HttpServletResponse response) throws IOException, RserveException, REXPMismatchException {
+			response.setContentType("text/json;charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			RConnection rconn = new RConnection("192.168.0.29");
+			rconn.setStringEncoding("utf8");
+
+			rconn.eval("source('C:/logs/final_test.R',encoding='UTF-8')");
+			// R의 계산 결과를 리스트로 리턴 받음(소스를 로딩하고 함수를 호출하는 과정, 어레이리스트아님)
+			RList list = rconn.eval("a3()").asList();
+
+
+			// 리스트의 첫 번째 요소를 double 배열로 리턴
+			double[] n1 = list.at(0).asDoubles();
+			double[] n2 = list.at(1).asDoubles();
+			double[] n3 = list.at(2).asDoubles();
+			
+			
+			JSONObject jo = new JSONObject();
+			JSONArray tdata = new JSONArray();
+			for(double num:n1) {
+				tdata.add(num);
+			}
+			jo.put("time",tdata);
+			
+			JSONArray tdata2 = new JSONArray();
+			for(double num:n2) {
+				tdata2.add(num);
+			}
+			jo.put("temp", tdata2);
+			
+			JSONArray tdata3 = new JSONArray();
+			for(double num:n3) {
+				tdata3.add(num);
+			}
+			jo.put("gas", tdata3);
+			
+			
+			out.print(jo.toJSONString());
+			out.close();
+			rconn.close();
+		}
+	
+	
+	
 }
 
 
